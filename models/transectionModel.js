e3042d7d2437d569dc2c61cf26344ba3553f1a51
KaifKhan07 <138020826+KaifKhan07@users.noreply.github.com>
2023-10-12 14:20:25 +0530

    deploy

---
+++ models/transectionModel.js
@@ -0,0 +1,69 @@
+const mongoose = require("mongoose");
+
+const transectionSchema = new mongoose.Schema(
+  {
+    userid: {
+      type: String,
+      required: true,
+    },
+    invoice_no: {
+      type: String,
+      required: [true, "invoice_no is required"],
+    },
+    name: {
+      type: String,
+      required: [true, "name is required"],
+    },
+    project: {
+      type: String,
+      required: [true, "project is required"],
+    },
+    amount: {
+      type: Number,
+      required: [true, "amount is required"],
+    },
+    type: {
+      type: String,
+      required: [true, "type is required"],
+    },
+    category: {
+      type: String,
+      requires: [true, "cat is required"],
+    },
+    refrence: {
+      type: String,
+    },
+    work_status: {
+      type: String,
+      required: [true, "work_status is required"],
+    },
+    phone_number: {
+      type: Number,
+      required: [true, "phone_number is required"],
+    },
+    bank_account: {
+      type: Number,
+      required: [true, "bank_account is required"],
+    },
+    branch: {
+      type: String,
+      required: [true, "branch is required"],
+    },
+    description: {
+      type: String,
+      required: [true, "desc is required"],
+    },
+    address: {
+      type: String,
+      required: [true, "addr is required"],
+    },
+    date: {
+      type: Date,
+      required: [true, "data is required"],
+    },
+  },
+  { timestamps: true }
+);
+
+const transectionModel = mongoose.model("transections", transectionSchema);
+module.exports = transectionModel;
